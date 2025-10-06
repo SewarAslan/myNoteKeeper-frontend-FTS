0de25829cab94d2efd727886b01beb50bd9732ce
SewarAslan <sewaraslan02@gmail.com>
2025-10-06 17:26:33 +0300

    feat: add NoteForm with expand/collapse, createNote API, and validation

--- src/services/api.js
+++ src/services/api.js
@@ -11,3 +11,21 @@ export const getAllNotes = async () => {
     throw error;
   }
 };
+
+export const createNote = async (note) => {
+  try {
+    const response = await fetch(API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(note),
+    });
+    if (!response.ok) throw new Error("adding new note FAILED!");
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error creating note:", error);
+    throw error;
+  }
+};
